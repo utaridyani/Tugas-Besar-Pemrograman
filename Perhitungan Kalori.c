414ce0e10d9797095428bf54a7deae2fbac8aa28
Herry Liukae <74525597+Gutema19-NIEL@users.noreply.github.com>
2020-12-04 02:10:59 +0800

    Update ke-1 Perhitungan Kalori.c
    Perubahan tampilan pada menu utama dan penambahan animasi loading serta penambahan comment

--- Perhitungan Kalori.c
+++ Perhitungan Kalori.c
@@ -1,9 +1,21 @@
 #include<stdio.h>
 #include<conio.h>
 
-int gender, age1,ask1;
-double mess, height;
-double hasil;
+int gender, age1;       /* Variabel untuk memasukkan nilai
+                        age1   = untuk memasukkan dan menyimpan nilai penentuan usia
+                        gender = untuk memasukkan dan menyimpan nilai penentuan jenis kelamin*/
+double mess, height;    /* Variabel untuk memasukkan nilai
+                        height = untuk memasukkan nilai tinggi badan
+                        mess   = untuk memasukkan nilai berat badan*/
+double hasil;           /* Variabel untuk memasukkan nilai 
+                        hasil = total nilai aritmatika dalam perhitungan kebutuhan kalori harian*/
+
+
+void koor(int x , int y);
+void bersih();
+void delay(int sec);
+void setcolor(unsigned short color);
+void Loading();
 
 double BMR();
 void utama();
@@ -11,6 +23,7 @@ void tanya();
 double TEE();
 
 int main(){
+  Loading();
   utama();
 }
 
@@ -22,16 +35,17 @@ void utama(){
                         puts("||         Kalori Harian           ||");
                         puts("||=================================||");
 
-    printf("+-----------------------------------------------------------------------+\n");
-    printf("|                                                                       |\n");
-    printf("+-----------------------------------------------------------------------+\n");
-    printf("|Tentukan jenis kelamin :                                               |\n");
-    printf("|                                                                       |\n");
-    printf("|       1.Laki-laki                                                     |\n");
-    printf("|       2.Perempuan                                                     |\n");
-    printf("|                                                                       |\n");
-    printf("|Tekan 1 untuk laki-laki atau 2 untuk perempuan pada jawaban anda       |\n");
-    printf("+-----------------------------------------------------------------------+\n");
+   printf("+-+-----------------------+-+\n");
+   printf("| |Tentukan jenis kelamin | |\n");
+   printf("+-+---------+-------------+-+\n");
+   printf("|1|Laki-laki|             | |\n");
+   printf("+-+---------+             | |\n");
+   printf("|2|Perempuan|             | |\n");
+   printf("+-+---------+-------------+-+\n");
+   printf("| |Tekan                  | |\n");
+   printf("| |(1) untuk laki-laki    | |\n");
+   printf("| |(2) untuk perempuan    | |\n");
+   printf("+-+-----------------------+-+\n");
     printf("|Jawaban anda : ");
     scanf ("%d",&gender);
     printf("Masukkan usia                  : ");
@@ -83,3 +97,43 @@ double TEE(){
       return bmr*1.725;
     }
 }
+void koor(int x , int y)
+{
+    HANDLE h;
+    COORD c;
+    c.X = x;
+    c.Y = y;
+    h = GetStdHandle(STD_OUTPUT_HANDLE);
+    SetConsoleCursorPosition(h,c);
+}
+void bersih()
+{
+    system("cls");
+}
+
+void delay(int sec)
+{
+    Sleep(sec);
+}
+
+void setcolor(unsigned short color)
+{
+    HANDLE hCon = GetStdHandle(STD_OUTPUT_HANDLE);
+    SetConsoleTextAttribute(hCon,color);
+}
+
+void Loading(){
+
+    bersih();
+    koor(40,9);
+    printf("||===============================||\n");
+    koor(40,10);
+    printf("||    Perhitungan Badan Ideal    ||\n");
+    koor(40,11);
+    printf("||===============================||\n");
+    koor(40,14);
+    for(int i=1 ; i<=34 ; i++){
+        setcolor(7);
+        printf("%c",219);
+        Sleep(60); } }
+
