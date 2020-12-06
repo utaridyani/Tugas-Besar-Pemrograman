ffbbd1c995fa49b332914787406e154743a2ed79
utaridyani <74400122+utaridyani@users.noreply.github.com>
2020-12-06 08:58:47 +0800

    Update fitur_tanyadokter.c

--- fitur_tanyadokter.c
+++ fitur_tanyadokter.c
@@ -1,74 +1,79 @@
 #include <stdio.h>
+#include <conio.h>
+#include <windows.h>
 
-int pDOKTER (int hari, int dokter);
-int menuPengguna (int pengguna, int userID, int password);
+//deklarasi fungsi
+int tanyaDokter (int hari, int dokter);
+void koor(int x , int y);
+void box(int a,int b,int c,int d);
+void box1(int a, int b, int c, int d);
+void bersih();
+void delay(int sec);
+void setcolor(unsigned short color);
 
 //Main Function
 int main(){
-int hari, dokter, pengguna, userID, password;
+    int hari, dokter, pengguna, userID, password;
+    int panjangX,panjangY,button;
 
-    menuPengguna(pengguna,userID,password);
-    pDOKTER(hari, dokter);
+    panjangX = 15;
+    panjangY = 12;
 
-}
-//Fungsi Jenis Pengguna
-int menuPengguna(int pengguna, int userID, int password){
-	printf("\t\t\tSelamat Datang di Weight Tracker!");
-	printf("\n\nMasuk sebagai : ");
-	printf("\n1. Pengguna Premium");
-	printf("\n2. Pengguna Reguler");
+    do{
+    tanyaDokter(hari, dokter);
 
-	printf("\n\nMasukkan nomor pilihan Anda : ");
-	scanf("%d",&pengguna);
+    koor(panjangX,panjangY);
+    printf(">>");
+    button = getch();
 
-	if (pengguna==1){
-		printf("User ID : ");
-		scanf("%d",&userID);
-		printf("Password : ");
-		scanf("%d",&password);
-
-		if (password=userID*11){
-            printf("Anda adalah pengguna premium\n");
-            //masukkan menu pengguna premium
-		}
-		else{
-            printf("Input Anda salah!");
-            return(0);
-		}
+    //72=tombol UP
+    //80=tombol DOWN
+    //13=ENTER
+    if(button == 80 && panjangY == 12)
+    {
+        panjangY = 13;
     }
-    else if(pengguna==2){
-        printf("Anda adalah pengguna reguler\n");
-        //Tampilkan menu pengguna reguler
+    else if(button == 80 && panjangY == 13)
+    {
+        panjangY = 14;
     }
-    else{
-        printf("Input yang anda masukkan salah!");
-        return(0);
+    else if(button == 80 && panjangY == 14)
+    {
+        panjangY = 15;
     }
-}
+    else if(button == 80 && panjangY == 15)
+    {
+        panjangY = 16;
+    }
+    else if(button == 72 && panjangY == 16)
+    {
+        panjangY = 15;
+    }
+    else if(button == 72 && panjangY == 15)
+    {
+        panjangY = 14;
+    }
+    else if(button == 72 && panjangY == 14)
+    {
+        panjangY = 13;
+    }
+    else if(button == 72 && panjangY == 13)
+    {
+        panjangY = 12;
+    }
+}while(button != 13);
 
-//Fungsi Tanya Dokter
-int pDOKTER (int hari, int dokter){
-	printf("\t\t\t+-----------------------+\n");
-	printf("\t\t\t|      Tanya Dokter     |\n");
-	printf("\t\t\t+-----------------------+\n");
-	printf("\nPilih hari untuk sesi tanya jawab : \n");
-	printf("1.Senin\n");
-	printf("2.Selasa\n");
-	printf("3.Rabu\n");
-	printf("4.Kamis\n");
-	printf("5.Jumat\n");
-	printf("\nMasukkan nomor hari pilihan Anda : ");
-	scanf("%d",&hari);
-
-	if (hari==1){
-		printf("\nAnda akan melakukan sesi tanya jawab pada hari Senin\n");
-		printf("Pilihan dokter yang bertugas pada hari Senin");
-		printf("\n\n1. dr.xxx");
-		printf("\nDokter spesialis x di Rumah Sakit x");
-		printf("\n\n2. dr. yyy");
-		printf("\nDokter spesialis y di Rumah Sakit y");
-		printf("\n\n3.dr.zzz");
-		printf("\nDokter di Klinik z");
+if(panjangX == 15 && panjangY == 12)
+{
+    bersih();
+        printf("\nAnda akan melakukan sesi tanya jawab pada hari Senin\n");
+        printf("Pilihan dokter yang bertugas pada hari Senin");
+        printf("\n\n1. dr.xxx");
+        printf("\nDokter spesialis x di Rumah Sakit x");
+        printf("\n\n2. dr. yyy");
+        printf("\nDokter spesialis y di Rumah Sakit y");
+        printf("\n\n3.dr.zzz");
+        printf("\nDokter di Klinik z");
 
 		printf("\n\nMasukkan nomor dokter pilihan Anda\n");
 		scanf("%d",&dokter);
@@ -93,10 +98,11 @@ int pDOKTER (int hari, int dokter){
             default :
                 printf("Input Anda salah!");
 		}
-	}
-
-	else if (hari==2){
-		printf("\nAnda akan melakukan sesi tanya jawab pada hari Selasa\n");
+}
+else if (panjangX == 15 && panjangY == 13)
+{
+    bersih();
+        printf("\nAnda akan melakukan sesi tanya jawab pada hari Selasa\n");
 		printf("\nPilihan dokter yang bertugas pada hari Selasa");
 		printf("\n\n1. dr.xxx");
 		printf("\nDokter spesialis x di Rumah Sakit x");
@@ -126,10 +132,11 @@ int pDOKTER (int hari, int dokter){
 				printf("Hubungi asisten dokter 1 jam sebelum tanya jawab pada nomor 081338879xxx");
 				printf("\nSelamat Melangsungkan Sesi Tanya Jawab!");
 		}
-	}
-
-	else if (hari==3){
-		printf("\nAnda akan melakukan sesi tanya jawab pada hari Rabu\n");
+}
+else if (panjangX == 15 && panjangY == 14)
+{
+    bersih();
+        printf("\nAnda akan melakukan sesi tanya jawab pada hari Rabu\n");
 		printf("\nPilihan dokter yang bertugas pada hari Rabu");
 		printf("\n\n1. dr.xxx");
 		printf("\nDokter spesialis x di Rumah Sakit x");
@@ -159,10 +166,11 @@ int pDOKTER (int hari, int dokter){
 				printf("Hubungi asisten dokter 1 jam sebelum tanya jawab pada nomor 081338879xxx");
 				printf("\nSelamat Melangsungkan Sesi Tanya Jawab!");
 		}
-	}
-
-		else if (hari==4){
-		printf("\nAnda akan melakukan sesi tanya jawab pada hari Kamis\n");
+}
+else if(panjangX == 15 && panjangY == 15)
+{
+    bersih();
+        printf("\nAnda akan melakukan sesi tanya jawab pada hari Kamis\n");
 		printf("\nPilihan dokter yang bertugas pada hari Kamis");
 		printf("\n\n1. dr.xxx");
 		printf("\nDokter spesialis x di Rumah Sakit x");
@@ -192,10 +200,11 @@ int pDOKTER (int hari, int dokter){
 				printf("Hubungi asisten dokter 1 jam sebelum tanya jawab pada nomor 081338879xxx");
 				printf("\nSelamat Melangsungkan Sesi Tanya Jawab!");
 		}
-	}
-
-		else if (hari==5){
-		printf("\nAnda akan melakukan sesi tanya jawab pada hari Jumat\n");
+}
+else if(panjangX == 15 && panjangY == 16)
+{
+    bersih();
+        printf("\nAnda akan melakukan sesi tanya jawab pada hari Jumat\n");
 		printf("\nPilihan dokter yang bertugas pada hari Jumat");
 		printf("\n\n1. dr.xxx");
 		printf("\nDokter spesialis x di Rumah Sakit x");
@@ -225,6 +234,92 @@ int pDOKTER (int hari, int dokter){
 				printf("Hubungi asisten dokter 1 jam sebelum tanya jawab pada nomor 081338879xxx");
 				printf("\nSelamat Melangsungkan Sesi Tanya Jawab!");
 		}
-	}}
+}
+getch();
+return(0);
+    }
 
+//Fungsi Tanya Dokter
+int tanyaDokter (int hari, int dokter){
+    bersih();
+	box(10,2,78,18);
+	box1(35,3,50,7);
+	koor(37,5); printf("TANYA DOKTER");
+	koor(12,10); printf("Pilih hari untuk sesi tanya jawab : ");
+	koor (18,12); printf("Senin");
+	koor (18,13); printf("Selasa");
+	koor (18,14); printf("Rabu");
+	koor (18,15); printf("Kamis");
+	koor (18,16); printf("Jumat");
 
+	printf("\n\n\n");
+}
+
+//Fungsi Untuk Menentukan letak kursor
+void koor(int x , int y)
+{
+    HANDLE h;
+    COORD c;
+    c.X = x;
+    c.Y = y;
+    h = GetStdHandle(STD_OUTPUT_HANDLE);
+    SetConsoleCursorPosition(h,c);
+}
+
+//Fungsi untuk membersihkan layar tanpa menutup program
+void bersih()
+{
+    system("cls");
+}
+
+//Fungsi Blocking yang menyebabkan program berhenti sejenak dan tidak melakukan proses
+void delay(int sec)
+{
+    Sleep(sec);
+}
+
+//Fungsi Untuk Memberi Warna
+void setcolor(unsigned short color)
+{
+    HANDLE hCon = GetStdHandle(STD_OUTPUT_HANDLE);
+    SetConsoleTextAttribute(hCon,color);
+}
+
+//Fungsi Pembetukan Box
+void box(int a,int b,int c,int d)
+{
+	int i,j;
+	for(i=a;i<c;i++)
+	{
+		koor(i,b);printf("\xcd");
+		koor(i,d);printf("\xcd");
+	}
+	for(j=b;j<d;j++)
+	{
+		koor(a,j);printf("\xba");
+		koor(c,j);printf("\xba");
+	}
+		koor(a,b);printf("\xc9");
+		koor(c,b);printf("\xbb");
+		koor(a,d);printf("\xc8");
+		koor(c,d);printf("\xbc");
+}
+
+void box1(int a,int b,int c,int d)
+{
+	int i,j;
+	for(i=a;i<c;i++)
+	{
+		koor(i,b);printf("%c",196);
+		koor(i,d);printf("%c",196);
+	}
+	for(j=b;j<d;j++)
+	{
+		koor(a,j);printf("%c",179);
+		koor(c,j);printf("%c",179);
+	}
+		koor(a,b);printf("%c",218);
+		koor(c,b);printf("%c",191);
+		koor(a,d);printf("%c",192);
+		koor(c,d);printf("%c",217);
+}
