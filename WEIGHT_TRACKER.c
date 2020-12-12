b0a5c8614fe9cdfed07883bd98af7b3e26d3040d
utaridyani <74400122+utaridyani@users.noreply.github.com>
2020-12-12 20:00:58 +0800

    Update WEIGHT_TRACKER.c

--- WEIGHT_TRACKER.c
+++ WEIGHT_TRACKER.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
-#include <conio.h>
 #include <windows.h>
+#include <string.h>
 
 //prototype
 void koor(int x , int y);
@@ -11,43 +11,72 @@ void delay(int sec);
 void setcolor(unsigned short color);
 void namaProgram();
 void pemilihanUser();
+void signup ();
+void login ();
+
+struct data
+{
+    char nama[100];
+    char username[20];
+    char password[10];
+    char noTLP[15];
+}a;
+char username[100];
+char password[10];
+int compare1;
+int compare2;
+FILE * fp;
 
 //Fungsi main
 int main()
 {
-    int panjangX,panjangY,button;
+    int panjangX,panjangY,button,kode;
 
-    box(7,5,76,22); //Memberikan garis pinggir
+    setcolor(10);
+    box(30,7,117,35);
     namaProgram();
 
-    panjangX = 6;
-    panjangY = 12;
+    panjangX = 37;
+    panjangY = 21;
 
 do{
     pemilihanUser();
 
     koor(panjangX,panjangY);
     printf("-->");
-    button = getch(); //apapun yang ditekan pada keyboard akan disimpan pada variabel button
+    button = getch();
 
     //Opsi untuk memindahkan user
-    if(button == 77 && panjangX == 6)
+    if(button == 77 && panjangX == 37)
     {
-        panjangX = 47;
+        panjangX = 94;
     }
-    else if(button == 75 && panjangX == 47)
+    else if(button == 75 && panjangX == 94)
     {
-        panjangX = 6;
+        panjangX = 37;
     }
 }while(button != 13);
-//User Permium
-if(panjangY == 12 && panjangX == 6)
+
+if(panjangY == 21 && panjangX == 37)
 {
     bersih();
-    //Menu Login
+    setcolor(10);
+    box(30,7,117,35);
+    box1(40,12,107,30);
+    setcolor(7);
+    koor(50,21); printf("MASUKAN KODE REGISTRASI : ");
+    scanf("%d", &kode); fflush(stdin);
+    if (kode==3748)
+    {
+        //menu login
+    }
+    else
+    {
+        koor(50,23);
+        printf("KODE LOGIN SALAH !");
+    }
 }
-//User Reguler
-else if(panjangY == 12 && panjangX == 47)
+else if(panjangY == 21 && panjangX == 94)
 {
     bersih();
    //Fitur-Fitur untuk user reguler
@@ -93,30 +122,33 @@ void setcolor(unsigned short color)
 //Tampilan Pertama nama program
 void namaProgram()
 {
-    box1(33,9,50,13); //memberi garis pinggir pada judul
-    koor(35,11);
-    printf("WEIGHT TRACKER");
-    koor(31,16);
-    printf("By Herry, Utari, Darryl");
-    koor(26,14);
+    setcolor(10);
+    box1(59,10,90,13);
+    setcolor(7);
+    koor(70,11); printf("W E I G H T");
+    koor(69,12); printf("T R A C K E R");
+    koor(75,28); printf("By");
+    koor(67,30); printf("Herry, Utari, Darryl");
+    koor(59,25);
     for(int i=1 ; i<=34 ; i++){
-        setcolor(7); //7 = simbol warna putih
+        setcolor(10);
         printf("%c",219);
-        Sleep(80); }
+        Sleep(10); }
 }
 
 //Fungsi Pemilihan Jenis User
 void pemilihanUser()
 {
     bersih();
-    box(2,5,70,16); //memberi garis pinggir
+    setcolor(10);
+    box(30,7,117,35);
+    box1(68,10,82,12);
+    box1(41,20,49,22);
+    box1(98,20,106,22);
     setcolor(7);
-    box1(28,7,42,9); //memberi garis pinggir pada tulisan
-    koor(29,8);printf("Jenis User : ");
-    box1(9,11,18,13); //memberi garis pinggir pada tulisan
-    koor(10,12);printf("Premium");
-    box1(50,11,58,13); //memberi garis pinggir pada tulisan
-    koor(51,12);printf("Reguler");
+    koor(70,11);printf("JENIS USER");
+    koor(42,21);printf("Premium");
+    koor(99,21);printf("Reguler");
 }
 
 //Fungsi Pembetukan Box
@@ -156,4 +188,54 @@ void box1(int a,int b,int c,int d)
 		koor(c,b);printf("%c",191);
 		koor(a,d);printf("%c",192);
 		koor(c,d);printf("%c",217);
+}
+
+void signup ()
+{
+    fp=fopen("data.txt", "a");
+
+    puts("Masukkan nama lengkap : ");
+    gets(a.nama);
+    puts("Masukkan username baru : ");
+    gets(a.username);
+    puts("Masukkan password : ");
+    gets(a.password);
+    puts("Masukkan nomor hp : ");
+    gets(a.noTLP);
+
+    fwrite(&a, sizeof(a), 1, fp);
+    fclose(fp);
+}
+
+void login ()
+{
+    fp=fopen("data.txt", "r");
+
+    printf("\nMasukkan username : ");
+    gets(username);
+    printf("\nMasukkan password : ");
+    gets(password);
+
+    fread(&a, sizeof(a), 1, fp);
+
+        compare1=strcmp(username, a.username);
+        compare2=strcmp(password, a.password);
+
+        if(compare1==0 && compare2!=0)
+        {
+            printf("Password anda salah!");
+        }
+        else if(compare1!=0 && compare2==0)
+        {
+            printf("Username anda salah!");
+        }
+        else if(compare1!=0 && compare2!=0)
+        {
+            printf("data anda salah!");
+        }
+        else if(compare1==0 && compare2==0)
+        {
+            printf("LOGIN SUCCES!");
+        }
+    fclose(fp);
 }
