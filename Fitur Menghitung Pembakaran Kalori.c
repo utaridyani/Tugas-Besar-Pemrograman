3e17f3c5dfd34b6ba4d9dfa86a4a9f2cd9a4dbaf
Herry Liukae <74525597+Gutema19-NIEL@users.noreply.github.com>
2020-12-14 00:53:17 +0800

    Update Fitur Menghitung Pembakaran Kalori.c
    Penambahan loading process, serta struct current date & time

--- Fitur Menghitung Pembakaran Kalori.c
+++ Fitur Menghitung Pembakaran Kalori.c
@@ -1,8 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include<windows.h>
+#include<time.h>
+#include<dos.h>
 
-int main(void)
+int tanggal;
+int bulan1;
+int tahun;
+int Jam;
+int Menit;
+int Detik;
+
+void koor(int x , int y);
+void bersih();
+void delay(int sec);
+void setcolor(unsigned short color);
+void Loading();
+void utama();
+
+void main(){
+    Loading();
+    utama();
+}
+
+
+void utama()
 {
+bersih();
+Tanggal_Waktu();
     printf("============================================================\n");
     printf("                    Menghitung Pembakaran Kalori            \n");
     printf("                         Saat Berolahraga                   \n");
@@ -89,3 +114,68 @@ int main(void)
 
   	return 0;
 }
+
+void Tanggal_Waktu(){
+    time_t current;
+    time(&current);
+
+    printf("%s", ctime(&current));
+
+    struct tm*local = localtime(&current);
+
+    tanggal = local->tm_mday;
+    bulan1  = local->tm_mon + 1;
+    tahun   = local->tm_year + 1900;
+    Jam     = local->tm_hour;
+    Menit   = local->tm_min;
+    Detik   = local->tm_sec;
+
+    if (Jam < 12)
+        printf("%02d:%02d:%02d\n", Jam,Menit,Detik);
+    else
+        printf("%02d:%02d:%02d\n", Jam-12,Menit,Detik);
+
+    printf("%02d/%02d/%d\n", tanggal,bulan1,tahun);
+}
+
+void koor(int x , int y)
+{
+    HANDLE h;
+    COORD c;
+    c.X = x;
+    c.Y = y;
+    h = GetStdHandle(STD_OUTPUT_HANDLE);
+    SetConsoleCursorPosition(h,c);
+}
+void bersih()
+{
+    system("cls");
+}
+
+void delay(int sec)
+{
+    Sleep(sec);
+}
+
+void setcolor(unsigned short color)
+{
+    HANDLE hCon = GetStdHandle(STD_OUTPUT_HANDLE);
+    SetConsoleTextAttribute(hCon,color);
+}
+
+void Loading(){
+
+    bersih();
+    koor(40,9);
+    printf("============================================================\n");
+    koor(40,10);
+    printf("                    Menghitung Pembakaran Kalori            \n");
+    koor(40,11);
+    printf("                         Saat Berolahraga                   \n");
+    koor(40,12);
+    printf("============================================================\n");
+    koor(40,14);
+    for(int i=1 ; i<=34 ; i++){
+        setcolor(7);
+        printf("%c",219);
+        Sleep(150); } }
